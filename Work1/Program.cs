9a9143297fea983ee373177f259985b060726dc5
ZHenya <saltykovazh@mail.ru>
2023-02-18 14:07:58 +0400

    solution Task

--- Work1/Program.cs
+++ Work1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите массив через пробел: ");
+
+string[] array = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+string[] result = new string[array.Length];
+
+int count = 0;
+
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        result[count] = array[i];
+        count += 1;
+    }
+}
+
+
+result = result[..^(array.Length - count)];
+WriteLine();
+WriteLine("Новый массив из строк, длина которых меньше или равна 3 : [{0}]", String.Join(",", result));
